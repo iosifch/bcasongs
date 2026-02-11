143026113d67dca2f7011101afd5907340b250ea
iosifch <iosifpub+gthb@gmail.com>
2026-02-11 10:33:25 +0200

    Refactor SongCard: use Vuetify Grid and fix title truncation

--- src/components/SongCard.vue
+++ src/components/SongCard.vue
@@ -5,52 +5,56 @@
     class="rounded-lg transition-swing"
     hover
   >
-    <v-card-text class="d-flex align-center justify-space-between py-3">
-      <div class="flex-grow-1 mr-2 min-width-0">
-        <div class="font-weight-regular text-h6 text-truncate">
-          {{ song.title }}
-        </div>
+    <v-card-text class="py-3">
+      <v-row no-gutters align="center" class="flex-nowrap">
+        <v-col class="overflow-hidden mr-2">
+          <div class="font-weight-regular text-h6 text-truncate">
+            {{ song.title }}
+          </div>
 
-        <div class="d-flex align-center mt-1">
-          <v-chip
-            color="secondary"
-            variant="tonal"
-            size="x-small"
-            class="font-weight-bold px-2"
-            label
-          >
-            {{ song.originalKey }}
-          </v-chip>
+          <div class="d-flex align-center mt-1">
+            <v-chip
+              color="secondary"
+              variant="tonal"
+              size="x-small"
+              class="font-weight-bold px-2"
+              label
+            >
+              {{ song.originalKey }}
+            </v-chip>
 
-          <span v-if="song.bookNumber" class="text-caption text-medium-emphasis ml-2">
-            #{{ song.bookNumber }}
-          </span>
-        </div>
-      </div>
+            <span v-if="song.bookNumber" class="text-caption text-medium-emphasis ml-2">
+              #{{ song.bookNumber }}
+            </span>
+          </div>
+        </v-col>
 
-      <div class="d-flex align-center">
-        <v-btn
-          icon
-          variant="text"
-          density="comfortable"
-          rounded="lg"
-          @click.prevent="emit('toggle-playlist', song.id)"
-        >
-          <v-icon :color="isInPlaylist ? 'primary' : 'grey-lighten-1'">
-            {{ isInPlaylist ? 'mdi-bookmark' : 'mdi-bookmark-outline' }}
-          </v-icon>
-        </v-btn>
+        <v-col cols="auto">
+          <div class="d-flex align-center">
+            <v-btn
+              icon
+              variant="text"
+              density="comfortable"
+              rounded="lg"
+              @click.prevent="emit('toggle-playlist', song.id)"
+            >
+              <v-icon :color="isInPlaylist ? 'primary' : 'grey-lighten-1'">
+                {{ isInPlaylist ? 'mdi-bookmark' : 'mdi-bookmark-outline' }}
+              </v-icon>
+            </v-btn>
 
-        <v-btn
-          icon
-          variant="text"
-          density="comfortable"
-          rounded="lg"
-          @click.prevent="emit('share', song)"
-        >
-          <v-icon color="grey-lighten-1">mdi-share-variant</v-icon>
-        </v-btn>
-      </div>
+            <v-btn
+              icon
+              variant="text"
+              density="comfortable"
+              rounded="lg"
+              @click.prevent="emit('share', song)"
+            >
+              <v-icon color="grey-lighten-1">mdi-share-variant</v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
